6ae2b0a59dda02e0c71cc2b57d83a0e8603ee287
Peter Kain <peter.kain@gmx.at>
2019-10-05 20:53:29 +0200

    Add modified blender files

--- blender/src/generate.py
+++ blender/src/generate.py
@@ -3,17 +3,33 @@ import math
 import random
 import os
 
+bpy.ops.object.select_all(action='DESELECT')
 
-def make_dirs(renderpath, infopath):
-    try:
-        os.makedirs(renderpath)
-    except OSError:
-        print("[RENDERPATH] found or failed to create")
-        
-    try:
-        os.makedirs(infopath)
-    except OSError:
-        print("[INFOPATH] found or failed to create")
+RENDERPATH = r"./renders/{}-{}.jpg"
+INFOPATH = r"./info/{}-{}.txt"
+TARGET_NAME = "Suzanne"
+ITERATIONS = 50
+
+TARGET_LOC = bpy.data.objects[TARGET_NAME].location
+
+tx = TARGET_LOC.x
+ty = TARGET_LOC.y
+tz = TARGET_LOC.z
+
+XMIN = -3
+XMAX = 3
+YMIN = int(ty - 8)
+YMAX = int(ty + 8)
+ZMIN = 2
+ZMAX = 5
+
+bpy.context.scene.render.image_settings.file_format = 'JPEG'
+
+try:
+    os.mkdir("renders/")
+    os.mkdir("info/")
+except OSError:
+    print("Directories available or failed")
     
 
 def center_camera_on_object(name):
@@ -24,77 +40,48 @@ def center_camera_on_object(name):
     bpy.context.object.constraints["Track To"].up_axis = 'UP_Y'
 
 
-def render(path, iteration, part):
-    bpy.context.scene.render.filepath = path.format(iteration, part)
+def render(iteration, part):
+    bpy.context.scene.render.filepath = RENDERPATH.format(iteration, part)
     bpy.ops.render.render(write_still=True)
     
 
-def add_camera(x, y, z, tname):
+def add_camera(x, y, z):
     bpy.ops.object.camera_add(align='VIEW', location=(x, y, z), rotation=(0,0,0))
     bpy.context.scene.camera = bpy.context.object
-    center_camera_on_object(tname)
+    center_camera_on_object(TARGET_NAME)
     
     
 def del_cameras():
     bpy.ops.object.select_by_type(type='CAMERA')
     bpy.ops.object.delete()
     
-    
-def get_shortest_dist(tname):
-    target = bpy.data.objects[tname]
-    target_world = target.matrix_world
-    target_msh = target.data
-    camera_loc = bpy.context.object.matrix_world.translation;
-    dists = [(target_world @ v.co - camera_loc).length for v in target_msh.vertices]
-    return min(dists)
-    
 
-def write_dist_info(path, tname, iteration, part):
-    file = open(path.format(iteration, part), "w+")
-    file.write(str(get_shortest_dist(tname)))
+def write_dist_info(iteration, part):
+    file = open(INFOPATH.format(iteration, part), "w+")
+    camera_loc = bpy.context.object.location;
+    cx = camera_loc.x
+    cy = camera_loc.y
+    cz = camera_loc.z
+    distance = math.sqrt((cx - tx) ** 2 + (cy - ty) ** 2 + (cz - tz) ** 2)
+    file.write(str(distance))
     file.close()
 
 
-# Generate for 1 object
-def generate_1(renderpath, infopath, target_name, iterations):
-    target_loc = bpy.data.objects[target_name].location
-
-    XMIN = -3
-    XMAX = 3
-    YMIN = int(target_loc.y - 8)
-    YMAX = int(target_loc.y + 8)
-    ZMIN = 2
-    ZMAX = 5
+for i in range(0, ITERATIONS):
+    # Calculate values
+    X = random.randint(XMIN, XMAX)
+    Y1 = random.randint(YMIN, YMAX)
+    Y2 = Y1 + 3 if Y1 > 0 else Y1 - 3
+    Z = random.randint(ZMIN, ZMAX)
     
-    RENDERPATH = renderpath + target_name
-    INFOPATH = infopath + target_name
-
-    RENDERFILEPATH = RENDERPATH + "/{}-{}.jpg"
-    INFOFILEPATH = INFOPATH + "/{}-{}.txt"
+    # Add first camera
+    add_camera(X, Y1, Z)
+    write_dist_info(i, 1)
+    render(i, 1)
+    del_cameras()
     
-    make_dirs(RENDERPATH, INFOPATH)
-    
-    for i in range(0, iterations):
-        # Calculate values
-        X = random.randint(XMIN, XMAX)
-        Y1 = random.randint(YMIN, YMAX)
-        Y2 = Y1 + 3 if Y1 > 0 else Y1 - 3
-        Z = random.randint(ZMIN, ZMAX)
-        
-        # Add first camera
-        add_camera(X, Y1, Z, target_name)
-        write_dist_info(INFOFILEPATH, target_name, i, 1)
-        render(RENDERFILEPATH, i, 1)
-        del_cameras()
-        
-        # Add second camera
-        add_camera(X, Y2, Z, target_name)
-        write_dist_info(INFOFILEPATH, target_name, i, 2)
-        render(RENDERFILEPATH, i, 2)
-        del_cameras()
-        
-
-# Generate for list of objects
-def generate_n(renderpath, infopath, target_names, iterations):
-    for tn in target_names:
-        generate_1(renderpath, infopath, tn, iterations)
+    # Add second camera
+    add_camera(X, Y2, Z)
+    write_dist_info(i, 2)
+    render(i, 2)
+    del_cameras()
--- blender/src/main.py
+++ blender/src/main.py
@@ -5,7 +5,7 @@ gen = bpy.data.texts["generate.py"].as_module()
 bpy.ops.object.select_all(action='DESELECT')
 bpy.context.scene.render.image_settings.file_format = 'JPEG'
 
-TARGET_NAMES = ["Suzanne.XS", "Suzanne.S", "Suzanne.M", "Suzanne.L", "Suzanne.XL"]
+TARGET_NAMES = ["Vase.XS", "Vase.S", "Vase.M", "Vase.L", "Vase.XL"]
 RENDERPATH = r"H:/DAData/renders/"
 INFOPATH = r"H:/DAData/info/"
 
