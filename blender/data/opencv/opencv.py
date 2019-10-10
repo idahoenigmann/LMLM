b2a259482034d16d10919db491d0a77e20ec78ff
Peter Kain <peter.kain@gmx.at>
2019-10-10 16:31:21 +0200

    Add saturated test images and update opencv.py

--- blender/data/opencv/opencv.py
+++ blender/data/opencv/opencv.py
@@ -1,9 +1,10 @@
 import cv2
 import os
+import numpy as np
 from argparse import ArgumentParser
 
-# -d renders/Suzanne.M -o greyscale
-options = ['greyscale']
+# -d ../renders/Suzanne.M -o saturated
+options = ['greyscale', 'saturated']
 
 parser = ArgumentParser(description='Manipulate images in directory')
 parser.add_argument('-d', help='the directory to process',
@@ -11,27 +12,42 @@ parser.add_argument('-d', help='the directory to process',
 parser.add_argument('-o', help='how to manipulate the images',
                     metavar='OPTION', dest='o', type=str)
 
+args = parser.parse_args()
+if args.o not in options:
+    print("Unknown option")
+    raise
+
+newdir_path = list(filter(None, args.d.split('/')))
+newdir_path[-1] += ('_{}'.format(args.o))
+newdir_path = '/'.join(newdir_path)
+
 try:
-    args = parser.parse_args()
-    if args.o not in options:
-        raise
-
-    newdir_path = list(filter(None, args.d.split('/')))
-    newdir_path[-1] += ('_{}'.format(args.o))
-    newdir_path = '/'.join(newdir_path)
     os.mkdir(newdir_path)
-    print('Will create files in: {}'.format(newdir_path))
-    for file in os.listdir(args.d):
-        filename = os.fsdecode(file)
-        if filename.endswith('.jpg'):
-            image = cv2.imread('{}/{}'.format(args.d, filename))
+except OSError:
+    print('Found {}'.format(newdir_path))
+    
+print('Will create files in: {}'.format(newdir_path))
+contents = os.listdir(args.d)
+for file in contents:
+    filename = os.fsdecode(file)
+    if filename.endswith('.jpg'):
+        image = cv2.imread('{}/{}'.format(args.d, filename))
 
-            if args.o == options[0]:
-                grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                cv2.imwrite('{}/{}'.format(newdir_path, filename), grey, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-         
-except:
-    print('First argument has to be a valid path\nSecond argument has to be one of the following:')
-    print(options)
-    exit(1)
+        if args.o == options[0]:
+            grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite('{}/{}'.format(newdir_path, filename), grey, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
+        if args.o == options[1]:
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV).astype('float32')
+            (h, s, v) = cv2.split(hsv)
+            s *= 1.5
+            s = np.clip(s, 0, 255)
+            hsv = cv2.merge([h, s, v])
+            saturated = cv2.cvtColor(hsv.astype('uint8'), cv2.COLOR_HSV2BGR)
+            cv2.imwrite('{}/{}'.format(newdir_path, filename), saturated, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+        else:
+            print('Unknown option: {}'.format(args.o))
+            print('Option has to be one of the following: ')
+            print(options)
+            exit(1)
