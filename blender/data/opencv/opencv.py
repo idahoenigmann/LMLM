01dafd2853ad8c57dd97bd9b9f425591d38d391a
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2019-10-24 10:26:39 +0200

    add cropping functionality

--- blender/data/opencv/opencv.py
+++ blender/data/opencv/opencv.py
@@ -4,7 +4,7 @@ import numpy as np
 from argparse import ArgumentParser
 
 # -d ../renders/Suzanne.M -o saturated
-options = ['greyscale', 'saturated', 'downscaled']
+options = ['greyscale', 'saturated', 'downscaled', 'cropping']
 
 parser = ArgumentParser(description='Manipulate images in directory')
 parser.add_argument('-d', help='the directory to process',
@@ -15,7 +15,7 @@ parser.add_argument('-o', help='how to manipulate the images',
 args = parser.parse_args()
 if args.o not in options:
     print("Unknown option")
-    raise
+    exit(1)
 
 newdir_path = list(filter(None, args.d.split('/')))
 newdir_path[-1] += ('_{}'.format(args.o))
@@ -33,11 +33,11 @@ for file in contents:
     if filename.endswith('.jpg'):
         image = cv2.imread('{}/{}'.format(args.d, filename))
 
-        if args.o == options[0]:
+        if args.o == options[0]:    # greyscale
             grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             cv2.imwrite('{}/{}'.format(newdir_path, filename), grey, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
-        if args.o == options[1]:
+        elif args.o == options[1]:    # saturated
             hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV).astype('float32')
             (h, s, v) = cv2.split(hsv)
             s *= 1.5
@@ -46,7 +46,7 @@ for file in contents:
             saturated = cv2.cvtColor(hsv.astype('uint8'), cv2.COLOR_HSV2BGR)
             cv2.imwrite('{}/{}'.format(newdir_path, filename), saturated, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
-        if args.o == options[2]:
+        elif args.o == options[2]:    # downscaled
             scale_percent = 10  # percent of original size
             width = int(image.shape[1] * scale_percent / 100)
             height = int(image.shape[0] * scale_percent / 100)
@@ -54,6 +54,13 @@ for file in contents:
 
             downscaled = cv2.resize(image, dim)
             cv2.imwrite('{}/{}'.format(newdir_path, filename), downscaled, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+        elif args.o == options[3]:    # cropping
+            crop_margin_percent = 5
+            crop_margin_width = int(image.shape[1] * crop_margin_percent / 100)
+            crop_margin_height = int(image.shape[0] * crop_margin_percent / 100)
+            crop_img = image[crop_margin_width:-crop_margin_width, crop_margin_height:-crop_margin_height]
+            cv2.imwrite('{}/{}'.format(newdir_path, filename), crop_img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
         else:
             print('Unknown option: {}'.format(args.o))
