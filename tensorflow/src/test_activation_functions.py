16b5559799eed94f9628a5b52c32ba48be92f041
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2019-11-18 16:03:26 +0100

    changes in test_activation_functions

--- tensorflow/src/test_activation_functions.py
+++ tensorflow/src/test_activation_functions.py
@@ -16,63 +16,62 @@ if __name__ == '__main__':
     exponential_l = []
     linear_l = []
 
-    test_e_l = []
+    x_axis = []
 
     with tf.Session() as sess:
-        for i in range(-25, 25):
-            # elu_l.append(tf.keras.activations.elu(float(i)).eval())
-            # selu_l.append(tf.keras.activations.selu(float(i)).eval())
-            # softplus_l.append(tf.keras.activations.softplus(float(i)).eval())
-            # softsign_l.append(tf.keras.activations.softsign(float(i)).eval())
-            # relu_l.append(tf.keras.activations.relu(float(i)).eval())
-            # tanh_l.append(tf.keras.activations.tanh(float(i)).eval())
-            # sigmoid_l.append(tf.keras.activations.sigmoid(float(i)).eval())
-            exponential_l.append(tf.keras.activations.exponential(float(i)).eval())
-            # linear_l.append(tf.keras.activations.linear(float(i)))
-            test_e_l.append(math.e ** i)
-    #
-    # plt.title("Exponential Linear Unit (ELU)")
-    # plt.plot(range(-25, 25), elu_l, label='elu')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_elu.png')
-    # plt.show()
-    #
-    # plt.title("Scaled Exponential Linear Unit (SELU)")
-    # plt.plot(range(-25, 25), selu_l, label='selu')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_selu.png')
-    # plt.show()
-    #
-    # plt.title("SoftPlus")
-    # plt.plot(range(-25, 25), softplus_l, label='softplus')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_softplus.png')
-    # plt.show()
-    #
-    # plt.title("Softsign")
-    # plt.plot(range(-25, 25), softsign_l, label='softsign')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_softsign.png')
-    # plt.show()
-    #
-    # plt.title("Rectified Linear Unit (ReLU)")
-    # plt.plot(range(-25, 25), relu_l, label='relu')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_relu.png')
-    # plt.show()
-    #
-    # plt.title("Tanh")
-    # plt.plot(range(-25, 25), tanh_l, label='tanh')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_tanh.png')
-    # plt.show()
-    #
-    # plt.title("Sigmoid")
-    # plt.plot(range(-25, 25), sigmoid_l, label='sigmoid')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_sigmoid.png')
-    # plt.show()
+        for i in range(-100, 100):
+            elu_l.append(tf.keras.activations.elu(float(i) / 20).eval())
+            selu_l.append(tf.keras.activations.selu(float(i) / 20).eval())
+            softplus_l.append(tf.keras.activations.softplus(float(i) / 20).eval())
+            softsign_l.append(tf.keras.activations.softsign(float(i) / 20).eval())
+            relu_l.append(tf.keras.activations.relu(float(i) / 20).eval())
+            tanh_l.append(tf.keras.activations.tanh(float(i) / 20).eval())
+            sigmoid_l.append(tf.keras.activations.sigmoid(float(i) / 20).eval())
+            exponential_l.append(tf.keras.activations.exponential(float(i) / 20).eval())
+            linear_l.append(tf.keras.activations.linear(float(i) / 20))
+            x_axis.append(float(i) / 20)
 
-    plt.title("Exponential")
-    plt.plot(range(-25, 25), exponential_l, label='exponential')
-    plt.plot(range(-25, 25), test_e_l, label='test')
-    #plt.savefig('methodology_neuralNetwork_activationFunction_exponential.png')
+    plt.title("Exponential Linear Unit (ELU)")
+    plt.plot(x_axis, elu_l, label='elu')
+    plt.savefig('methodology_neuralNetwork_activationFunction_elu.png')
+    plt.show()
+    #
+    plt.title("Scaled Exponential Linear Unit (SELU)")
+    plt.plot(x_axis, selu_l, label='selu')
+    plt.savefig('methodology_neuralNetwork_activationFunction_selu.png')
+    plt.show()
+    #
+    plt.title("SoftPlus")
+    plt.plot(x_axis, softplus_l, label='softplus')
+    plt.savefig('methodology_neuralNetwork_activationFunction_softplus.png')
+    plt.show()
+    #
+    plt.title("Softsign")
+    plt.plot(x_axis, softsign_l, label='softsign')
+    plt.savefig('methodology_neuralNetwork_activationFunction_softsign.png')
+    plt.show()
+    #
+    plt.title("Rectified Linear Unit (ReLU)")
+    plt.plot(x_axis, relu_l, label='relu')
+    plt.savefig('methodology_neuralNetwork_activationFunction_relu.png')
+    plt.show()
+    #
+    plt.title("Tanh")
+    plt.plot(x_axis, tanh_l, label='tanh')
+    plt.savefig('methodology_neuralNetwork_activationFunction_tanh.png')
+    plt.show()
+    #
+    plt.title("Sigmoid")
+    plt.plot(x_axis, sigmoid_l, label='sigmoid')
+    plt.savefig('methodology_neuralNetwork_activationFunction_sigmoid.png')
     plt.show()
 
-    # plt.title("Linear")
-    # plt.plot(range(-25, 25), linear_l, label='linear')
-    # plt.savefig('methodology_neuralNetwork_activationFunction_linear.png')
-    # plt.show()
+    plt.title("Exponential")
+    plt.plot(x_axis, exponential_l, label='exponential')
+    plt.savefig('methodology_neuralNetwork_activationFunction_exponential.png')
+    plt.show()
+
+    plt.title("Linear")
+    plt.plot(x_axis, linear_l, label='linear')
+    plt.savefig('methodology_neuralNetwork_activationFunction_linear.png')
+    plt.show()
