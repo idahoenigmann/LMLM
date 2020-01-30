dc47e81ab8d8e581f3afb0f88bdc38976fe5ae53
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2020-01-30 17:19:30 +0100

    add steps_per_epoch

--- tensorflow/src/xor.py
+++ tensorflow/src/xor.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
-input_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
-output_data = [[0], [1], [1], [0]]
+input_data = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
+output_data = [[0.0], [1.0], [1.0], [0.0]]
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(2, name="dense_1", input_shape=(2,), activation=tf.keras.activations.sigmoid),
@@ -12,4 +12,12 @@ model = tf.keras.Sequential([
 model.summary()
 
 model.compile(loss=tf.keras.losses.mean_absolute_error, learning_rate=0.15)
-model.fit(input_data, output_data, epochs=10000)
+
+for i in range(1000):
+    model.fit(input_data, output_data, epochs=100, verbose=0)
+
+    idx = 0
+    for actual in model.predict(input_data):
+        print("expected: {}; actual: {}".format(output_data[idx], actual))
+        idx += 1
+    print()
